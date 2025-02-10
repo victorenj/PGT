ed1d700dd8a64149a557bf056fcf52e8239158fe
Victor Emmanuel Balasoto <28060675+victorenj@users.noreply.github.com>
2025-02-10 09:26:05 -0500

    Group players per hole

--- scorecard_stableford.py
+++ scorecard_stableford.py
@@ -13,6 +13,12 @@ import streamlit as st
 import pandas as pd
 
 
+st.set_page_config(
+    page_title="PGT Scorecard",
+    page_icon=":golfer:",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
 def main():
     '''Create scorecard'''
@@ -28,10 +34,9 @@ def main():
     
     # Title of the app
     st.logo("assets/pgt_logo2_blk.jpg", size="large")
-    st.title("PGT Stableford Scorecard")
-    st.subheader("*Pinoy Golf Tour*")
+    st.title("PGT Modified Stableford")
     
-    # Sidebar for player input
+    # --- Sidebar for player input ---
     st.sidebar.header("Player Information")
     num_players = st.sidebar.number_input("Number of Players", min_value=1, max_value=4, value=1)
     player_names = []
@@ -39,17 +44,17 @@ def main():
     for i in range(num_players):
         player_name = st.sidebar.text_input(f"Enter name for Player {i + 1}", value=f"Player {i + 1}")
         player_names.append(player_name)
-    
-    # Sidebar for course input
+    # --- Sidebar for player input ---
+    # --- Sidebar for course input ---
     course_name = ["Knights Play", "Brevofield", "Quaker Creek", "Raleigh GA", "Zebulon CC", "Custom"]
     golf_course = st.sidebar.selectbox("Golf Course", course_name)
 
     if golf_course == "Custom":
         custom_input = st.sidebar.text_input("Write the name of Golf Course :")
         if custom_input:
-            st.write(f'You are playing :  **_{custom_input} Golf Course_**')
+            st.subheader(f'**_{custom_input} Golf Course_**')
     else:
-        st.write(f'You are playing :  **_{golf_course} Golf Course_**')
+        st.subheader(f'**_{golf_course} Golf Course_**')
     
     if golf_course == "Knights Play":
         st.sidebar.write(kp)
@@ -62,29 +67,23 @@ def main():
     elif golf_course == "Zebulon CC":
         st.sidebar.write(zc)
 
-    if golf_course == 'Knights Play':
-        holes = [f"Hole {i}" for i in range(1, 10)]  # List of holes (1 to 9)
-    else:
-        holes = [f"Hole {i}" for i in range(1, 19)]  # List of holes (1 to 18)
-    
-    # Initialize a DataFrame to store scores and points
+    # --- Hole & total scores ---
+    holes = [f"Hole {i}" for i in range(1, 19)]  # List of holes (1 to 18)
     score_value = pd.DataFrame(index=holes, columns=player_names)
-    
-    # Input scores for each hole and player
-    columns = st.columns(4, gap="small", vertical_alignment="center")
+    columns = st.columns(18, gap="small", vertical_alignment="top")
     
     options = {
         'Zero': 0,
         'Bogey': 1,
         'Par': 2,
-        'Birdie': 3,
-        'Eagle': 4
+        'Birdie': 4,
+        'Eagle': 6
     }
     
-    for player, column in zip(player_names, columns):
+    for hole, column in zip(holes, columns):
         with column:
-            st.subheader(f'{player}')
-            for hole in holes:
+            #st.subheader(f'{player}')
+            for player in player_names:
                 score_key = st.selectbox(f"{hole} ({player})", options.keys())
                 score_value.loc[hole, player] = options[score_key]
     
@@ -98,8 +97,8 @@ def main():
         st.subheader("Total Scores")
         total_scores = score_value.sum(axis=0)
         for player, total in total_scores.items():
-            #st.subheader(f"Score for {player}")
             st.write(f"{player}: **{total}**")
-        
+    # --- Hole & total scores ---
+
 if __name__ == '__main__':
     main()
